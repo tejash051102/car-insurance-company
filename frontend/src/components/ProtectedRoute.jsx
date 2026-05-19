0c4c64e888350d43920542ebd26bc172a008ad85
Tejash Sharma <sharmatejash868@gmail.com>
2026-05-19 19:29:10 +0530

    uodate

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,11 +1,18 @@
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 import { getAuthUser } from "../utils/authStorage.js";
 
 const ProtectedRoute = ({ children }) => {
   const userInfo = getAuthUser();
+  const location = useLocation();
 
   if (!userInfo) {
-    return <Navigate to="/login" replace state={{ from: location }} />;
+    return (
+      <Navigate
+        to="/login"
+        replace
+        state={{ from: `${location.pathname}${location.search}${location.hash}` }}
+      />
+    );
   }
 
   return children;
